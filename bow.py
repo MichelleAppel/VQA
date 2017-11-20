42d2426383949bfac092f5013fec287873e26319
Nils Hulzebosch <Nils@wcw-stud-145-109-59-12.wireless.uva.nl>
2017-11-20 14:07:32 +0100

    Reading data from data_reader.py

--- bow.py
+++ bow.py
@@ -6,9 +6,10 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import numpy as np
+from data_reader import read_textual_data
 
 # STEP 0: define hyperparameters
-NUM_EPOCHS = 100
+NUM_EPOCHS = 10
 LEARNING_RATE = 0.1
 RNDM_SEED = 42
 
@@ -16,9 +17,18 @@ torch.manual_seed(RNDM_SEED) # set random seed for continuity
 
 
 # STEP 1: read in data
-#TODO methods for reading in data in seperate file    
-             
-data = [
+q_train, q_valid, q_test, a_train, a_valid, a_test = read_textual_data()
+
+train_data = []
+for i in range(len(q_train)):
+    train_data.append((q_train[i].split(), a_train[i]))
+
+test_data = []
+for i in range(len(q_test)):
+    test_data.append((q_test[i].split(), a_test[i]))
+
+"""
+train_data = [
     ('What English meal is this likely for?'.split(), 'tea'),
     ('What insurance company is a sponsor?'.split(), 'state farm'),
     ('Is there a bell on the train?'.split(), 'yes')
@@ -28,8 +38,8 @@ test_data = [
     ('What English meal is this likely for?'.split(), 'tea'),
     ('What insurance company is a sponsor?'.split(), 'state farm'),
     ('Is there a bell on the train?'.split(), 'yes')
-] 
-            
+]
+"""
 
 # STEP 2: create source_vocabulary and target_vocabulary
 
@@ -38,7 +48,7 @@ test_data = [
 source_vocabulary = {}
 target_vocabulary = {}
 target_vocabulary_lookup = []
-for sent, label in data + test_data:
+for sent, label in train_data + test_data:
     for word in sent:
         if word not in source_vocabulary:
             source_vocabulary[word] = len(source_vocabulary)
@@ -87,38 +97,40 @@ bow_model = BoWClassifier(NUM_LABELS, VOCAB_SIZE)
 
 #for param in bow_model.parameters():
 #    print(param)
-
 # To run the model, pass in a BoW vector, but wrapped in an autograd.Variable
-sample = data[0]
-bow_vector = make_bow_vector(sample[0], source_vocabulary)
-log_probs = bow_model(autograd.Variable(bow_vector))
-print(log_probs)
-
-
-
+#sample = data[0]
+#bow_vector = make_bow_vector(sample[0], source_vocabulary)
+#log_probs = bow_model(autograd.Variable(bow_vector))
+#print(log_probs)
 
 # Run on test data before we train, just to see a before-and-after
-for instance, label in test_data:
-    bow_vec = autograd.Variable(make_bow_vector(instance, source_vocabulary))
-    log_probs = bow_model(bow_vec)
-    print(log_probs)
+#for instance, label in test_data:
+#    bow_vec = autograd.Variable(make_bow_vector(instance, source_vocabulary))
+#    log_probs = bow_model(bow_vec)
+#    print(log_probs)
 
-# intialize loss function (= Negative Log Likelihood Loss) and optimizer (= Stochastic Gradient Descent)
+
+# intialize loss function (= Negative Log Likelihood Loss)
 loss_function = nn.NLLLoss()
+
+# intialize optimizer (= Stochastic Gradient Descent)
 optimizer = optim.SGD(bow_model.parameters(), lr=LEARNING_RATE)
 
+
+j = 0
 # train (= update parameters) for 100 epochs
 #TODO improve training by including k-fold cross validation
-for epoch in range(NUM_EPOCHS):
-    for instance, label in data:
-    
+for epoch in range(1):
+    #print("EPOCH:", epoch, "/ 2")
+    for instance, label in train_data:
+        print(j, "/", len(train_data))
         # clear previous gradients
         bow_model.zero_grad()
 
         # create BoW vector (features) and target (label)
         bow_vec = autograd.Variable(make_bow_vector(instance, source_vocabulary))
         target = autograd.Variable(make_target(label, target_vocabulary))
-
+        
         # run forward pass: compute log probabilities and loss
         log_probs = bow_model(bow_vec)
         loss = loss_function(log_probs, target)
@@ -126,9 +138,13 @@ for epoch in range(NUM_EPOCHS):
         # run backward pass: compute gradients and update parameters with optimizer.step()
         loss.backward()
         optimizer.step()
+        
+        j += 1
 
+counter = 0 
 #TODO calculate accuracy on test_data
 for instance, label in test_data:
+    counter += 1
     bow_vec = autograd.Variable(make_bow_vector(instance, source_vocabulary))
     log_probs = bow_model(bow_vec)
     value, index = torch.max(log_probs, 1)
@@ -136,6 +152,8 @@ for instance, label in test_data:
     prediction = target_vocabulary_lookup[index]
     
     _, label = test_data[index]
+    
+    #TODO take the highest X probabilities to get the best X predictions (instead of 1)
     
     if prediction == label:
         print("hell yes")
@@ -147,4 +165,5 @@ for instance, label in test_data:
     print("INDEX:", index)
     print("BEST LABEL:", prediction) 
     print("\n\n")
-    
+    if counter == 10:
+        break
