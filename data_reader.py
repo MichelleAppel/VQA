70382da9c5a8550ab7008de2670cbe56e27bec42
Nils Hulzebosch <Nils@wcw-stud-145-109-59-12.wireless.uva.nl>
2017-11-20 14:07:14 +0100

    Improved methods

--- data_reader.py
+++ data_reader.py
@@ -2,38 +2,67 @@ import h5py
 import json
 import numpy as np
 
-#TODO:  THIS FILE READS IN ALL DATA AND PASSES IT TO THE MAIN FILE WHEN CALLED
+def read_image_data():
+    # load image features from hdf5 file and convert it to numpy array
+    img_features = np.asarray(h5py.File('data/VQA_image_features.h5', 'r')['img_features'])
+
+    # load mapping file
+    with open('data/VQA_img_features2id.json', 'r') as f:
+        visual_feat_mapping = json.load(f)['VQA_imgid2id']
+
+    # example how to access image features
+    #img_id = 262145
+    #h5_id = visual_feat_mapping[str(img_id)]
+    #img_feat = img_features[h5_id]
+    #print(img_feat)
+
+def read_textual_data():
+    with open("data/vqa_questions_train", 'r') as f:
+        questions_train = [x['question'] for x in json.load(f)['questions']]
+    with open("data/vqa_questions_valid", 'r') as f:
+        questions_valid = [x['question'] for x in json.load(f)['questions']]
+    with open("data/vqa_questions_test", 'r') as f:
+        questions_test = [x['question'] for x in json.load(f)['questions']]
+    with open("data/vqa_annotations_train", 'r') as f:
+        annotations_train = [x['multiple_choice_answer'] for x in json.load(f)['annotations']]
+    with open("data/vqa_annotations_valid", 'r') as f:
+        annotations_valid = [x['multiple_choice_answer'] for x in json.load(f)['annotations']]
+    with open("data/vqa_annotations_test", 'r') as f:
+        annotations_test = [x['multiple_choice_answer'] for x in json.load(f)['annotations']]
+    
+    return questions_train, questions_valid, questions_test, annotations_train, annotations_valid, annotations_test
 
 
-# load image features from hdf5 file and convert it to numpy array
-img_features = np.asarray(h5py.File('data/VQA_image_features.h5', 'r')['img_features'])
-
-# load mapping file
-with open('data/VQA_img_features2id.json', 'r') as f:
-     visual_feat_mapping = json.load(f)['VQA_imgid2id']
-
-# example how to access image features
-img_id = 262145
-h5_id = visual_feat_mapping[str(img_id)]
-img_feat = img_features[h5_id]
 
 
-with open("data/vqa_annotations_train", 'r') as f:
-    annotations_train = json.load(f)['annotations']
-with open("data/vqa_annotations_valid", 'r') as f:
-    annotations_valid = json.load(f)['annotations']
-with open("data/vqa_annotations_test", 'r') as f:
-    annotations_test = json.load(f)['annotations']
-with open("data/vqa_questions_train", 'r') as f:
-    questions_train = json.load(f)['questions']
-with open("data/vqa_questions_valid", 'r') as f:
-    questions_valid = json.load(f)['questions']
-with open("data/vqa_questions_test", 'r') as f:
-    questions_test = json.load(f)['questions']
-       
-print("annotations_train example:", annotations_train[0], "\n")
-print("annotations_valid example:", annotations_valid[0], "\n")
-print("annotations_test example:", annotations_test[0], "\n")
-print("questions_train example:", questions_train[0], "\n")
-print("questions_valid example:", questions_valid[0], "\n")
-print("questions_test example:", questions_test[0], "\n")
+"""
+ONE EXAMPLE OF ANNOTATIONS DATA
+{
+'question_type': 'what',
+'multiple_choice_answer': 'tea',
+'answers': [
+    {'answer': 'brunch', 'answer_confidence': 'maybe', 'answer_id': 1},
+    {'answer': 'tea', 'answer_confidence': 'yes', 'answer_id': 2},
+    {'answer': 'tea time', 'answer_confidence': 'yes', 'answer_id': 3},
+    {'answer': 'brunch', 'answer_confidence': 'yes', 'answer_id': 4},
+    {'answer': 'breakfast', 'answer_confidence': 'maybe', 'answer_id': 5},
+    {'answer': 'tea', 'answer_confidence': 'yes', 'answer_id': 6},
+    {'answer': 'teatime', 'answer_confidence': 'yes', 'answer_id': 7},
+    {'answer': 'lunch', 'answer_confidence': 'yes', 'answer_id': 8},
+    {'answer': 'reception', 'answer_confidence': 'maybe', 'answer_id': 9},
+    {'answer': 'breakfast', 'answer_confidence': 'yes', 'answer_id': 10}
+    ],
+'image_id': 228478,
+'answer_type': 'other',
+'question_id': 228478002,
+'split': 'train'
+} 
+
+ONE EXAMPLE OF QUESTIONS DATA
+{
+'image_id': 228478,
+'question': 'What English meal is this likely for?',
+'question_id': 228478002,
+'split': 'train'
+} 
+"""
